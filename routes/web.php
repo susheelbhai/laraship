9b41962321dff460c2912728a91f01c9434e5a87
Susheel Kumar Singh <susheelkrsingh306@gmail.com>
2023-10-03 16:40:37 +0530

    further developnent

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Routing\Route;
+use Illuminate\Support\Facades\Route;
 use Susheelbhai\Larapay\Http\Controllers\ShipmentController;
 
 Route::get('checkBalance', [ShipmentController::class, 'checkBalance'])->name('checkBalance');
